9fd41d87674fd6842836196de837c5c88af14ce1
janett g <janettxgonzalez@gmail.com>
2018-08-19 00:12:03 -0400

    jquery exercise

---
+++ exercise.js
@@ -0,0 +1,11 @@
+//select all divs with a background of purple
+$("div").css("background", "purple");
+
+//select the div with class and it 200px width
+$(".highlight").css("width", "200px");
+
+//select the div if and give it a orange border
+$("#third").css("border", "2px solid orange");
+
+//select the first div only and change the font color to pink
+$("div:first-of-type").css("color", "pink");
